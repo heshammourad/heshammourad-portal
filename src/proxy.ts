30c7f2bf737b07954ba0da4a2663d11e819edb3e
Hesham Mourad <heshammourad@gmail.com>
2026-07-15 02:09:49 +0000

    fix: exclude /spotify-tools from NextAuth proxy matcher to prevent infinite redirect loops

--- src/proxy.ts
+++ src/proxy.ts
@@ -25,10 +25,11 @@ export const config = {
     /*
      * Match all request paths except for the ones starting with:
      * - api (API routes)
+     * - spotify-tools (public sub-project)
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+    "/((?!api|spotify-tools|_next/static|_next/image|favicon.ico).*)",
   ],
 };
